6cb106f25aca1354f47d418c7e997003b5782f81
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-11-11 15:08:43 +0200

    Minor refactoring

--- src/main/java/com/fs12/javaspringboot/user/UserRepository.java
+++ src/main/java/com/fs12/javaspringboot/user/UserRepository.java
@@ -2,6 +2,6 @@ package com.fs12.javaspringboot.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
