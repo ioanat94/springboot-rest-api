f28d7b674e3f20f6d638272c119fc3f29a213224
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-11-11 16:09:00 +0200

    Create Order controller

--- src/main/java/com/fs12/javaspringboot/order/OrderController.java
+++ src/main/java/com/fs12/javaspringboot/order/OrderController.java
@@ -1,6 +1,5 @@
 package com.fs12.javaspringboot.order;
 
-import com.fs12.javaspringboot.product.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
