0a692131abc3568f125525aea37f8b4e377d3091
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-11-11 14:00:02 +0200

    Create Order repository

--- src/main/java/com/fs12/javaspringboot/order/Order.java
+++ src/main/java/com/fs12/javaspringboot/order/Order.java
@@ -1,18 +1,46 @@
 package com.fs12.javaspringboot.order;
 
-import com.fs12.javaspringboot.product.Product;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
 public class Order {
+    @Id
+    @SequenceGenerator(
+            name = "order_sequence",
+            sequenceName = "order_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "order_sequence"
+    )
+    private Long id;
     private Date date;
-    private Status status;
-    private String user;
-    private Product[] products;
+    private String status;
+    private String customer;
+    @ElementCollection(targetClass = String.class)
+    @Column(name="products", nullable=false)
+    @CollectionTable(name = "orders_products", joinColumns = @JoinColumn(name = "order_id", referencedColumnName = "id"))
+    private List<String> products;
     private double totalPrice;
     private String shippingAddress;
+
+    public Order(Date date, String status, String customer, List<String> products, double totalPrice, String shippingAddress) {
+        this.date = date;
+        this.status = status;
+        this.customer = customer;
+        this.products = products;
+        this.totalPrice = totalPrice;
+        this.shippingAddress = shippingAddress;
+    }
 }
--- src/main/java/com/fs12/javaspringboot/order/Status.java
+++
@@ -1,5 +0,0 @@
-package com.fs12.javaspringboot.order;
-
-public enum Status {
-    PROCESSING, CONFIRMED, SHIPPING, DELIVERED
-}
