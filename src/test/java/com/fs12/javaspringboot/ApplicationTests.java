960068cec560c07a78d89716ea27c8e0b148741e
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-11-11 10:08:49 +0200

    Init

---
+++ src/test/java/com/fs12/javaspringboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fs12.javaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
