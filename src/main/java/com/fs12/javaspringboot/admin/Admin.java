4063969139e0748fa5d9869e011cc7ade83f1992
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-11-11 15:07:14 +0200

    Create Admin repository

--- src/main/java/com/fs12/javaspringboot/admin/Admin.java
+++ src/main/java/com/fs12/javaspringboot/admin/Admin.java
@@ -2,13 +2,40 @@ package com.fs12.javaspringboot.admin;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "admins")
 public class Admin {
+    @Id
+    @SequenceGenerator(
+            name = "admin_sequence",
+            sequenceName = "admin_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "admin_sequence"
+    )
+    private int id;
     private String firstName;
     private String lastName;
-    private final String email;
+    private String email;
     private String password;
-    private Permission[] permissions;
+    @ElementCollection(targetClass = Permission.class)
+    private List<Permission> permissions;
+
+    public Admin(String firstName, String lastName, String email, String password, List<Permission> permissions) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.permissions = permissions;
+    }
 }
---
+++ src/main/java/com/fs12/javaspringboot/admin/AdminRepository.java
@@ -0,0 +1,6 @@
+package com.fs12.javaspringboot.admin;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
+}
