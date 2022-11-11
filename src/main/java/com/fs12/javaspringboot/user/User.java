a995f07d6bb9bde26341dcf1984643108244f313
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-11-11 14:51:28 +0200

    Create User repository

--- src/main/java/com/fs12/javaspringboot/user/User.java
+++ src/main/java/com/fs12/javaspringboot/user/User.java
@@ -2,13 +2,38 @@ package com.fs12.javaspringboot.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private int id;
     private String firstName;
     private String lastName;
-    private final String email;
+    private String email;
     private String image;
     private boolean isBanned;
+
+    public User(String firstName, String lastName, String email, String image, boolean isBanned) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.image = image;
+        this.isBanned = isBanned;
+    }
 }
